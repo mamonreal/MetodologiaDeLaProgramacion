0d1e7cf4339109791c6694230c9d3425c95396e0
mamonreal <ma.monreal@alumnos.urjc.es>
2017-03-02 18:45:58 +0100

    añadidos metodos de Polyline figure, todavía no terminado.

--- src/uHotDrawFigures/PolylineFigure.java
+++ src/uHotDrawFigures/PolylineFigure.java
@@ -1,6 +1,9 @@
 package uHotDrawFigures;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.Rectangle;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,4 +22,35 @@ public class PolylineFigure extends AbstractFigure{
 		lp.add(p2);
 		lp.add(p3);
 	}
+	
+	public void remove(Point p) {
+		lp.remove(p);
+	}
+
+	@Override
+	public Rectangle displayBox() {
+		// TODO Auto-generated method stub
+		return super.displayBox();
+	}
+
+	@Override
+	public boolean containsPoint(double x, double y) {
+		// TODO Auto-generated method stub
+		return super.containsPoint(x, y);
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		g.setColor(Color.RED);
+		for(int i = 0; i<lp.size()-1; i++)
+			g.drawLine(lp.get(i).x, lp.get(i).y, lp.get(i+1).x, lp.get(i+1).y);
+	}
+
+	@Override
+	public void moveBy(Point p) {
+		// TODO Auto-generated method stub
+		super.moveBy(p);
+	}
+	
+	
 }
